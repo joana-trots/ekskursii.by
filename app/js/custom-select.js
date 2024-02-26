04535162ae1f65cf7bc9d438641329031904b6b9
joana-trots <jana.trots@gmail.com>
2024-02-26 15:47:58 +0300

    upd routes

--- app/js/custom-select.js
+++ app/js/custom-select.js
@@ -46,7 +46,6 @@ var x, i, j, l, ll, selElmnt, a, b, c, notGenerate, val;
                 if (selElmnt.options[j].selected) {
                     c.setAttribute('class', 'same-as-selected');
                 }
-                console.log(selElmnt.options[j].disabled);
                 c.addEventListener('click', function(e) {
                     /* When an item is clicked, update the original select box,
                     and the selected item: */
