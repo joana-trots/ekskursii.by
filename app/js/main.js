9877417d368ee1e394783f023bbca3fbdae913b0
joana-trots <jana.trots@gmail.com>
2024-02-29 12:44:09 +0300

    upd js

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
 /* QUICK NAV */
 const qsection = document.getElementById('qsection'),
-      qnavbar = document.getElementById('qnavbar');
+      qnavbar = document.getElementById('qnavbar'),
+      sections = document.querySelectorAll('.about__inner section, section.route, section.feedback');
 
 var qlink = document.querySelectorAll('.quicknav__tags-group--tag-link'),
-    qlinkActive = document.querySelector('.quicknav__active-link'),
-    qid = document.querySelectorAll('.about__inner section, section.route, section.feedback');
+    qlinkActive = document.querySelector('.quicknav__active-link');
 
 window.onscroll = function() {
   if ((window.scrollY - qsection.offsetTop) > 0) {
@@ -12,37 +12,51 @@ window.onscroll = function() {
   } else {
     qnavbar.classList.remove('onscroll');
   }
+
+
+
+
+  let options = { 
+    root: qsection, 
+    threshold: 0.5 
+  }  
+  let callback = function(entries) { 
+    entries.forEach(entry => { 
+      if (entry.isIntersecting) { 
+  
+          const target = entry.target;
+  
+          sections.forEach((section => {
+            id = section.getAttribute('id');
+            // console.log(id.getAttribute('id'));
+            qlink.forEach((link => {
+              href = link.getAttribute('href').substring(1);
+              // console.log(href);
+              if (href === id) {
+                console.log('true');
+                link.classList.add('quicknav__active-link');
+              }
+              else link.classList.add('quicknav__active-link');
+            }));
+            
+          }));
+          
+          console.log(target) 
+      }
+    }); 
+  }
+  const observer = new IntersectionObserver(callback, options); 
+  sections.forEach(section => { 
+    observer.observe(section); 
+  });
+
+  
 };   
 
 
-const sections = document.querySelectorAll('.about__inner section');
-let options = { 
-  root: qsection, 
-  threshold: 0.5 
-}  
-let callback = function(entries) { 
-  entries.forEach(entry => { 
-    if (entry.isIntersecting) { 
-        const target = entry.target;
 
-        qid.forEach((id => {
-          id.getAttribute('id');
-          // console.log(id.getAttribute('id'));
-          qlink.forEach((link => {
-            href = link.getAttribute('href').substring(1);
-            // console.log(href);
-            if (id === href) {console.log('true')}
-          }));
-        }));
 
-        console.log(target) 
-    } 
-  }); 
-}
-const observer = new IntersectionObserver(callback, options); 
-sections.forEach(section => { 
-  observer.observe(section); 
-});
+
 
 /* SCHEDULE TOGGLE CARDS\TABLE */
 var scheduleDisplay = document.querySelectorAll('.schedule__display'),
