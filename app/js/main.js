4e2c20ea095250a1f235e47a7e8cfef3eaa75e9e
joana-trots <jana.trots@gmail.com>
2024-03-06 11:11:23 +0300

    add form validation

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,22 @@
-var formField = document.querySelectorAll('.booking-form__form-field:has(.booking-form__form-field--input):has(input:invalid)');
-formField.forEach((field => {
-  var input = document.querySelectorAll('.booking-form__form-field--input input');
-  input.forEach((item) => {
-    item.onblur = function(){
-      if (item.checkValidity()) {
-        field.classList.add('invalid');
-        console.log(field);
-      }
-    }
-  })
-}))
+// var formField = document.querySelectorAll('.booking-form__form-field'),
+//     submitBtn = document.querySelectorAll('[type="submit"]');
 
+// formField.forEach((field => {
+//   var input = field.querySelectorAll('input');
+//   input.forEach((item) => {
+//     item.onblur = function(e){
+//       const isValid = e.target.checkValidity();
+//       if (!isValid) field.classList.add('invalid');
+//       else field.classList.remove('invalid');
+//     }
+//   });
+// }))
+// submitBtn.forEach(btn => {
+//   btn.onclick = function(e) {
+//     e.preventDefault();
+    
+//   }
+// })
 /* SCHEDULE TOGGLE CARDS\TABLE */
 var scheduleDisplay = document.querySelectorAll('.schedule__display'),
     scheduleButton  = document.querySelectorAll('.schedule__options-item--display');
