a356176d47621a1414fd990e5d2413706c4458cc
joana-trots <jana.trots@gmail.com>
2024-04-18 14:34:58 +0300

    upd _navonpage.js

--- app/js/_navonpage.js
+++ app/js/_navonpage.js
@@ -3,28 +3,42 @@
   const sections = document.querySelectorAll(".quicknav__section, #route, #feedback");
   const links = document.querySelectorAll(".quicknav__tags-group--tag-link");
   const setActiveLink = (id) => {
-    links.forEach((link) => {
-      if (link.getAttribute("href") === `#${id}`) {
-        link.classList.add("tags-group__link--active");
-      } else {
-        link.classList.remove("tags-group__link--active");
-      }
-    });
+      links.forEach((link) => {
+          if (link.getAttribute("href") === `#${id}`) {
+              link.classList.add("tags-group__link--active");
+          } else {
+              link.classList.remove("tags-group__link--active");
+          }
+      });
   };
-  let options = {
-    root: null,
-    threshold: 0.5,
-  };
-  let callback = function (entries) {
-    entries.toReversed().forEach((entry) => {
-      if (entry.isIntersecting) {
-        const target = entry.target;
-        setActiveLink(target.getAttribute("id"));
-      }
-    });
-  };
-  const observer = new IntersectionObserver(callback, options);
+  const observer = new IntersectionObserver(function (entries) {
+      entries.toReversed().forEach((entry) => {
+          const heightMiddle = window.innerHeight / 2;
+          if (
+              entries[0].target.getBoundingClientRect().top - 50 < heightMiddle
+              && entries[0].target.getBoundingClientRect().top + 50 > heightMiddle
+          ) {
+              setActiveLink(entry.target.getAttribute("id"));
+          }
+      });
+  }, {
+      root: null,
+      threshold: 0,
+      rootMargin: '50% 0px -50% 0px'
+  });
+
   sections.forEach((section) => {
-    observer.observe(section);
+      observer.observe(section);
   });
 })();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth',
+          block: "center"
+      });
+  });
+});
