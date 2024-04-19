7334fea491e96aad3fc754eb02f63be7d13d44f1
joana-trots <jana.trots@gmail.com>
2024-04-19 10:14:49 +0300

    upd

--- app/js/_navonpage.js
+++ app/js/_navonpage.js
@@ -1,44 +1,62 @@
-/* QUICK NAV */
-(function () {
-  const sections = document.querySelectorAll(".quicknav__section, #route, #feedback");
-  const links = document.querySelectorAll(".quicknav__tags-group--tag-link");
-  const setActiveLink = (id) => {
-      links.forEach((link) => {
-          if (link.getAttribute("href") === `#${id}`) {
-              link.classList.add("tags-group__link--active");
-          } else {
-              link.classList.remove("tags-group__link--active");
-          }
-      });
-  };
-  const observer = new IntersectionObserver(function (entries) {
-      entries.toReversed().forEach((entry) => {
-          const heightMiddle = window.innerHeight / 2;
-          if (
-              entries[0].target.getBoundingClientRect().top - 50 < heightMiddle
-              && entries[0].target.getBoundingClientRect().top + 50 > heightMiddle
-          ) {
-              setActiveLink(entry.target.getAttribute("id"));
-          }
-      });
-  }, {
-      root: null,
-      threshold: 0,
-      rootMargin: '50% 0px -50% 0px'
-  });
+// Подсветка
+    // ---------------------------------------------------------------------------------------
+    (function () {
+        const sections = document.querySelectorAll(".quicknav__section, #route, #feedback");
+        const links = document.querySelectorAll(".quicknav__tags-group .quicknav__tags-group--tag-link");
+        const setActiveLink = (id) => {
+            links.forEach((link) => {
+                if (link.getAttribute("href") === `#${id}`) {
+                    link.classList.add("tags-group__link--active");
+                } else {
+                    link.classList.remove("tags-group__link--active");
+                }
+            });
+        };
+        let activeTag = null;
+        let lastScroll = 0;
+        document.addEventListener('scroll', (event) => {
+            if(window.scrollY > lastScroll){
+                sections.forEach((section) => {
+                    if (
+                        section.getBoundingClientRect().top - 50 < 200
+                        && section.getBoundingClientRect().top + 50 > 200
+                        && activeTag !== section.getAttribute("id")
+                    ) {
+                        activeTag = section.getAttribute("id");
+                        setActiveLink(section.getAttribute("id"));
+                    }
+                });
+            }else{
+                sections.forEach((section) => {
+                    if (
+                        section.getBoundingClientRect().bottom - 50 < window.innerHeight - 200
+                        && section.getBoundingClientRect().bottom + 50 > window.innerHeight - 200
+                        && activeTag !== section.getAttribute("id")
+                    ) {
+                        activeTag = section.getAttribute("id");
+                        setActiveLink(section.getAttribute("id"));
+                    }
+                });
+            }
+            lastScroll = window.scrollY;
+        });
+    })();
 
-  sections.forEach((section) => {
-      observer.observe(section);
-  });
-})();
+    // Плавная прокрутка
+    // ---------------------------------------------------------------------------------------
+    document.querySelectorAll('.quicknav__tags-group a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
 
-document.querySelectorAll('.quicknav__tags-group--tag-link[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+            let href = this.getAttribute('href').substring(1);
+            const scrollTarget = document.getElementById(href);
+            const topOffset = 200;
+            const elementPosition = scrollTarget.getBoundingClientRect().top;
+            const offsetPosition = elementPosition - topOffset;
 
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth',
-          block: "center"
-      });
-  });
-});
+            window.scrollBy({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        });
+    });
